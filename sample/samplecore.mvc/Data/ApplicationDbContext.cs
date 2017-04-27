966c1325d76380e433a983f16d40db4bae55bc6c
Dave Melendez <dave_melendez@hotmail.com>
2017-04-26 21:39:28 -0700

    sample mvc app

---
+++ sample/samplecore.mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using samplecore.mvc.Models;
+using ElCamino.AspNetCore.Identity.DocumentDB;
+using ElCamino.AspNetCore.Identity.DocumentDB.Model;
+
+namespace samplecore.mvc.Data
+{
+    public class ApplicationDbContext : IdentityCloudContext
+    {
+        public ApplicationDbContext(IdentityConfiguration config)
+            : base(config)
+        {
+        }
+
+        
+    }
+}
---
+++ sample/samplecore.mvc/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace samplecore.mvc.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
