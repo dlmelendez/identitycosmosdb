41eddb2f481e8a7850be8d5992fe70c107ec0f68
David Melendez <dave_melendez@hotmail.com>
2025-12-21 09:27:29 -0600

    IDE0130 suppress inline

--- src/ElCamino.AspNetCore.Identity.CosmosDB/IdentityCosmosDBBuilderExtensions.cs
+++ src/ElCamino.AspNetCore.Identity.CosmosDB/IdentityCosmosDBBuilderExtensions.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Identity;
 using ElCamino.AspNetCore.Identity.CosmosDB.Model;
 using ElCamino.AspNetCore.Identity.CosmosDB;
 
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace Microsoft.Extensions.DependencyInjection
+#pragma warning restore IDE0130 // Namespace does not match folder structure
 {
 	public static class IdentityCosmosDBBuilderExtensions
 	{
