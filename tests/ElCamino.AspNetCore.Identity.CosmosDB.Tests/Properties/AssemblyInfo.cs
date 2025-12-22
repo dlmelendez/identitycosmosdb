edf4afa83bc17969ca00b9af669e7be2544524af
David Melendez <dave_melendez@hotmail.com>
2025-12-21 19:13:32 -0600

    Disable parallel tests in ci/cd builds

--- tests/ElCamino.AspNetCore.Identity.CosmosDB.Tests/Properties/AssemblyInfo.cs
+++ tests/ElCamino.AspNetCore.Identity.CosmosDB.Tests/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+#if DEBUG
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+#else
+// Parallel execution disabled for Release builds
+[assembly: Parallelize(Scope = ExecutionScope.ClassLevel, Workers = 1)]
+#endif
